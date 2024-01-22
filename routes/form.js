77e98aa2b9469c14a63db033748839d6b23e5228
Raymond Zeng <shilei.zeng98@outlook.com>
2024-01-21 21:12:08 -0500

    Seperated form router from index router

--- routes/form.js
+++ routes/form.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/new', function(req, res, next) {
-  res.render('new', { title: 'Mini Message Board'})
+/* GET form page. */
+router.get('/form', function(req, res, next) {
+  res.render('form', { title: 'Mini Message Board'});
+});
+
+/* POST form page. */
+router.post('/form', function(req, res, next) {
+  messages.push({
+    user_name: req.body.user_name,
+    message: req.body.message,
+    date: DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS),
+  })
+  res.redirect('/');
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -13,19 +13,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Message Board', messages: messages});
 });
 
-/* GET form page. */
-router.get('/form', function(req, res, next) {
-  res.render('form', { title: 'Mini Message Board'});
-});
 
-/* POST form page. */
-router.post('/form', function(req, res, next) {
-  messages.push({
-    user_name: req.body.user_name,
-    message: req.body.message,
-    date: DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS),
-  })
-  res.redirect('/');
-});
 
 module.exports = router;
