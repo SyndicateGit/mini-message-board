1050ce9d0d09307776f8cb87580cac3a8a172b39
Raymond Zeng <shilei.zeng98@outlook.com>
2024-01-21 21:33:10 -0500

    Fixed bug where form needs access to index messages

--- routes/form.js
+++ routes/form.js
@@ -6,14 +6,5 @@ router.get('/form', function(req, res, next) {
   res.render('form', { title: 'Mini Message Board'});
 });
 
-/* POST form page. */
-router.post('/form', function(req, res, next) {
-  messages.push({
-    user_name: req.body.user_name,
-    message: req.body.message,
-    date: DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS),
-  })
-  res.redirect('/');
-});
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Message Board', messages: messages});
 });
 
+/* POST form page. 
+Included here so it can access messages.*/
+router.post('/form', function(req, res, next) {
+  messages.push({
+    user_name: req.body.user_name,
+    message: req.body.message,
+    date: DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS),
+  })
+  res.redirect('/');
+});
 
 
 module.exports = router;
